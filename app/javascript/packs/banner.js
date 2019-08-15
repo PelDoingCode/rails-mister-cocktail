25a5a75226c620c17bb7b47068606c572ce000af
Julien Pelegri <pelegrijulien@hotmail.fr>
2019-08-15 22:14:07 +0100

    styling a bit

--- app/javascript/packs/banner.js
+++ app/javascript/packs/banner.js
@@ -3,7 +3,7 @@ import Typed from 'typed.js';
 function loadDynamicText() {
   new Typed('#banner-typed-text', {
     strings: ["Need a cocktail?", "Select a cocktail ! ", "And come order one ! "],
-    typeSpeed: 50,
+    typeSpeed: 70,
     loop: true
   });
 }
