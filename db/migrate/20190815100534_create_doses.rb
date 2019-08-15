bc0d7f88a443b088b51cbc2994b37b507a2524c0
Julien Pelegri <pelegrijulien@hotmail.fr>
2019-08-15 11:16:34 +0100

    db initialized

---
+++ db/migrate/20190815100534_create_doses.rb
@@ -0,0 +1,11 @@
+class CreateDoses < ActiveRecord::Migration[5.2]
+  def change
+    create_table :doses do |t|
+      t.text :description
+      t.bigint :cocktail_id
+      t.bigint :ingredient_id
+
+      t.timestamps
+    end
+  end
+end
