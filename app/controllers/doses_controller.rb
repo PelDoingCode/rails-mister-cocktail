1b1d2e5b0f65968642831d5e9499d9ca65f110e4
Julien Pelegri <pelegrijulien@hotmail.fr>
2019-08-15 12:01:14 +0100

    doses controller

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,10 @@
+class DosesController < ApplicationController
+  def create
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  get 'doses/create'
+  get 'doses/destroy'
+  get 'doses/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'cocktails#index'
-  resources :cocktails, only: %i[index show new create destroy]
+  resources :cocktails, only: %i[index show new create destroy] do
+    resources :doses, only: [:create]
+  end
+  resources :doses, only: %i[destroy edit]
 end
